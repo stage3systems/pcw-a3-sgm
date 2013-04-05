be083bc142ce8f136cb6b813fd4762f3e51cb4e9
Evax Software <contact@evax.fr>
2013-04-05 19:15:31 +0200

    Add pdf export to published PDFA page

--- app/controllers/disbursments_controller.rb
+++ app/controllers/disbursments_controller.rb
@@ -1,6 +1,7 @@
 class DisbursmentsController < ApplicationController
   #before_filter :authenticate_user!, :except => :published
   add_breadcrumb "Proforma Disbursments", :disbursments_url
+  include ActionView::Helpers::NumberHelper
 
   # GET /disbursments
   # GET /disbursments.json
@@ -18,7 +19,140 @@ class DisbursmentsController < ApplicationController
     @revision = @published.current_revision rescue nil
     respond_to do |format|
       format.html { render layout: 'published' }
+      format.pdf {
+        file = Rails.root.join 'pdfs', "#{@revision.reference}.pdf"
+        #unless  File.exists? file
+          format_published_pdf
+          @pdf.render_file file
+        #end
+        send_file(file, :type => "application/pdf")
+      }
     end
+  end
+
+  def format_pdf_assert_space(n)
+    if @gridy+n >= @nrows
+      @pdf.grid([@gridy,0],[@gridy+n-1,@ncols-1]).bounding_box do
+        @pdf.cell :width => @pdf.bounds_width, :height => @pdf.bounds.height,
+                  :border_width => 0
+      end
+      @gridy = 0
+      @pdf.start_new_page
+    end
+  end
+
+  def format_published_pdf
+    @pdf = Prawn::Document.new :page_layout => :portrait
+    @pdf.font_size = 6
+    @pdf.font_families.update(
+      "DejaVuSans" => { :bold => Rails.root.join('fonts', 'DejaVuSans-Bold.ttf').to_s,
+                        :normal => Rails.root.join('fonts', 'DejaVuSans.ttf').to_s })
+    @pdf.font "DejaVuSans"
+    @nrows = 72
+    @ncols = 12
+    @pdf.define_grid :columns => @ncols, :rows => @nrows, :gutter => 0
+    @gridy = 0
+    @pdf.fill_color = '000000'
+    format_pdf_assert_space 10
+    logo_path = Rails.root.join('app', 'assets', 'images', 'monson_agency.png')
+    @pdf.grid([0,0], [9, 5]).bounding_box do
+      @pdf.text "PROFORMA DISBURSMENT", :size => 12, :style => :bold,
+                :align => :left, :valign => :center
+    end
+    @pdf.grid([0, 6], [9, @ncols-1]).bounding_box do
+      @pdf.image logo_path, :width => 40, :position => :right, :vposition => :center
+    end
+    @gridy += 10
+    format_pdf_assert_space 18
+    @pdf.grid([@gridy, 0], [@gridy+18, 5]).bounding_box do
+      @pdf.fill_color = '123123'
+      data = [
+        ['<b>To</b>', "<b>#{@revision.data['company_name']}</b>\n#{@revision.data['company_email']}"],
+        ['<b>Reference</b>', @revision.reference],
+        ['<b>Issued</b>', I18n.l(@revision.updated_at)],
+        ['<b>Vessel<b>', "#{@published.vessel_name} <font size=\"4.5\">(GRT: #{@revision.data["vessel_grt"]} | NRT: #{@revision.data["vessel_nrt"]} | DWT: #{@revision.data["vessel_dwt"]} | LOA: #{@revision.data["vessel_loa"]})</font>"],
+        ['<b>Port</b>', @published.port.name],
+        ['<b>Cargo Type</b>', (@revision.cargo_type.display rescue "N/A")],
+        ['<b>Cargo Quantity</b>', @revision.cargo_qty],
+        ['<b>Load Time</b>', @revision.loadtime],
+        ['<b>Tugs</b>', "#{@revision.tugs_in} In - #{@revision.tugs_out} Out"]
+      ]
+      @pdf.table data,
+                 cell_style: {border_widths: [0.1, 0, 0, 0], inline_format: true},
+                 column_widths: [70, 200]
+    end
+    @pdf.grid([@gridy, 7], [@gridy+18, @ncols-1]).bounding_box do
+      data = [
+        ['<b>From</b>', "<b>Monson Agencies Australia Pty Ltd</b>\n7/96 Toolooa Street\nGladstone, QLD, 4680"]
+      ]
+      @pdf.table data,
+                 cell_style: {border_widths: [0.1, 0, 0, 0], inline_format: true},
+                 column_widths: [70, 155]
+    end
+    @gridy += 18
+    format_pdf_assert_space 1
+    column_widths = [180, 135, 225]
+    if @revision.tax_exempt?
+      column_widths = [315, 225]
+    end
+    @pdf.grid([@gridy, 0], [@gridy+1, @ncols-1]).bounding_box do
+      data = [
+        ['<b>Item</b>', "<b>Amount (#{@revision.data["currency_code"]})</b>", "<b>Amount (#{@revision.data["currency_code"]}) Including Taxes</b>"]
+      ]
+      if @revision.tax_exempt?
+        data = data.map {|d| d.slice(0,2)}
+      end
+      @pdf.table data,
+                 cell_style: {border_widths: [0, 0, 0, 0], height: 10,
+                              padding: 1, inline_format: true},
+                 column_widths: column_widths
+    end
+    @gridy += 1
+    @revision.field_keys.each do |f|
+      format_pdf_assert_space 1
+      @pdf.grid([@gridy, 0], [@gridy+1, @ncols-1]).bounding_box do
+        data = [
+          [@revision.descriptions[f], number_to_currency(@revision.values[f]), number_to_currency(@revision.values_with_tax[f])]
+        ]
+        if @revision.tax_exempt?
+          data = data.map {|d| d.slice(0,2)}
+        end
+        @pdf.table data,
+                   cell_style: {border_widths: [0.1, 0, 0, 0], height: 10,
+                                padding: 1, inline_format: true},
+                   column_widths: column_widths
+      end
+      @gridy += 1
+    end
+    format_pdf_assert_space 2
+    @pdf.grid([@gridy, 0], [@gridy+2, @ncols-1]).bounding_box do
+      if @revision.tax_exempt?
+        data = [
+          ['<b>Total</b>', "<b><font size=\"16\">#{number_to_currency(@revision.data['total'])}</font></b>"]
+        ]
+      else
+        data = [
+          ['<b>Total</b>', "<b>#{number_to_currency(@revision.data["total"])}<b>",
+                           "<b><font size=\"14\">#{number_to_currency(@revision.data['total_with_tax'])}</font></b>"]
+        ]
+      end
+      @pdf.table data, 
+                 cell_style: {border_widths: [0.1, 0, 0, 0], height: 20,
+                              padding: 1, inline_format: true},
+                 column_widths: column_widths
+    end
+    @gridy += 2
+    @pdf.move_down(20)
+    @pdf.table [['']],
+               cell_style: {border_widths: [0.1, 0, 0, 0]},
+               column_widths: [540]
+    @pdf.text "Please remit funds at least two (2) days prior to vessels arrival to the following Bank Account:\n\n<b>ANZ Bank Ltd</b>\n<b>7 Queen Street</b>\n<b>Fremantle, Western Australia</b>\n\n<b>SWIFT Code:ANZBAU3M</b>\n<b>BSB Number:016-307</b>\n<b>A/C Number:1077-33669</b>\n<b>A/C Name: Monson Agencies Australia Pty Ltd</b>\n<b>Reference: Vessel Name</b>\n\nDisclaimer: this is only an estimate and any additional costs incurred for this vessel will be accounted for in our Final D/A.\n\nThis estimate is exclusive of Australian Freight Tax (AFT) which, if applicable, shall be paid by the freight beneficiary, ie owner/disponent owner.\n\n", inline_format: true
+    if @revision.tax_exempt?
+      @pdf.text "This estimate is exclusive of Australian Goods and Services Tax (GST).  The GST portion will be claimed back from the Australian Tax Office on your behalf.\n\n", inline_format: true
+    else
+      @pdf.text "This estimate is inclusive of Australian Goods and Services Tax (GST).\n\n", inline_format: true
+    end
+    @pdf.text "Dowload the full <link href=\"https://monson-disbursments.evax.fr/MAA%20Standard%20Terms%20and%20Conditions%20Sep%202012.pdf\">Terms and Conditions</link>", inline_format: true
   end
 
   def publish
