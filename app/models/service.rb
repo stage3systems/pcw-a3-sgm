cdcc9170e0b2c7c9db01eea7072107aaaec81446
Evax Software <contact@evax.fr>
2013-05-28 09:29:36 +0200

    Ensure service codes are checked for syntax and runtime errors

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,10 +3,26 @@ class Service < ActiveRecord::Base
   attr_accessor :changelog
   attr_accessible :user_id, :code, :item, :key,
                   :port_id, :row_order, :terminal_id, :document, :compulsory
+  validate :code_checks
   belongs_to :port
   belongs_to :terminal
   belongs_to :user
   has_many :service_updates
   mount_uploader :document, TariffUploader
   ranks :row_order, :with_same => [:port_id, :terminal_id]
+
+  def code_checks
+    ctx = V8::Context.new
+    begin
+      ctx.eval("var c = #{self.code};")
+    rescue Exception => e
+      errors.add(:code, "Syntax Error: #{e.message}")
+      return
+    end
+    begin
+      ctx.eval("c.compute({})")
+    rescue Exception => e
+      errors.add(:code, "Runtime Error: #{e.message}")
+    end
+  end
 end
--- test/unit/service_test.rb
+++ test/unit/service_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ServiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "code must be syntactically correct" do
+    s = Service.new
+    s.item = "Test"
+    s.key = "TST"
+    s.code = "{compute: function(ctx) return 0; }, taxApplies: false}"
+    assert !s.save, "Cannot save bad code"
+    assert_not_nil s.errors[:code]
+    assert s.errors[:code][0].start_with? "Syntax Error: "
+  end
+
+  test "code must pass basic runtime checks" do
+    s = Service.new
+    s.item = "Test"
+    s.key = "TST"
+    s.code = "{compute: function(ctx) { return parseBoson(ctx.boson); }, taxApplies: false}"
+    assert !s.save, "Cannot save bad code"
+    assert_not_nil s.errors[:code]
+    assert s.errors[:code][0].start_with? "Runtime Error: "
+  end
+
+  test "valid code passes" do
+    s = Service.new
+    s.item = "Test"
+    s.key = "TST"
+    s.code = "{compute: function(ctx) { return 0; }, taxApplies: false}"
+    assert s.save, "Can save good code"
+  end
 end
