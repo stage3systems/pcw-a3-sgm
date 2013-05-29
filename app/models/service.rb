6b26ea3719534263023b4e6239cc2bc76ed6bcc4
Evax Software <contact@evax.fr>
2013-05-29 20:12:59 +0200

    Use a seemingly valid context mockup for service code testing

--- app/models/service.rb
+++ app/models/service.rb
@@ -11,8 +11,36 @@ class Service < ActiveRecord::Base
   mount_uploader :document, TariffUploader
   ranks :row_order, :with_same => [:port_id, :terminal_id]
 
+  def mockup_context
+    <<CTX
+    var ctx = {
+      vessel: {
+        nrt: #{Random.rand(100000.0).round(2)},
+        grt: #{Random.rand(100000.0).round(2)},
+        dwt: #{Random.rand(100000.0).round(2)},
+        loa: #{Random.rand(300.0).round(1)}
+      },
+      estimate: {
+        eta: new Date(#{"\""}#{DateTime.now}#{"\""}),
+        cargo_qty: #{Random.rand(50000.0).round(2)},
+        tugs_in: #{Random.rand(10)},
+        tugs_out: #{Random.rand(10)},
+        loadtime: #{Random.rand(100)},
+        days_alongside: #{Random.rand(3)}
+      },
+      cargo_type: {
+        type: "",
+        subtype: "",
+        subsubtype: "",
+        subsubsubtype: ""
+      }
+    };
+CTX
+  end
+
   def code_checks
     ctx = V8::Context.new
+    ctx.eval(mockup_context)
     begin
       ctx.eval("var c = #{self.code};")
     rescue Exception => e
@@ -20,7 +48,7 @@ class Service < ActiveRecord::Base
       return
     end
     begin
-      ctx.eval("c.compute({})")
+      ctx.eval("c.compute(ctx)")
     rescue Exception => e
       errors.add(:code, "Runtime Error: #{e.message}")
     end
