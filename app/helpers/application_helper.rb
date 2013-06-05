69ba823b88fbac8aab808349cd1b0416f6590877
Evax Software <contact@evax.fr>
2013-06-05 11:23:30 +0200

    Configuration tab submenu and aquila-like footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def icon(name)
     "<i class=\"icon-#{name}\"></i>".html_safe
   end
+
+  def configuration_tab?
+    not ["home", "disbursments"].member? controller_name
+  end
+
+  def configuration_url
+    return ports_url if current_user.admin?
+    vessels_url
+  end
+
+  def ports_active?
+    ['ports', 'tariffs', 'services', 'terminals'].member? controller_name
+  end
 end
