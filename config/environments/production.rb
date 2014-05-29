3588064ef192507e4f485b18a13b08a4485aa617
Evax Software <contact@evax.fr>
2014-05-29 17:19:00 +0200

    Precompile tenant specific assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ ProformaDA::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( da_desk.css monson.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
