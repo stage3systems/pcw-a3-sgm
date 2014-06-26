69376c314fcac2a6308a4686b926cf70dcbd211c
Evax Software <contact@evax.fr>
2014-06-26 16:31:24 +0200

    Remove unused application helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,11 +11,6 @@ module ApplicationHelper
     not ["home", "disbursements"].member? controller_name
   end
 
-  def configuration_url
-    return ports_url if current_user.admin?
-    vessels_url
-  end
-
   def nan_to_zero(n)
     return 0 if n == 'NaN'
     return n
