399a29d00cc1caa0e9f6c0b4d80294778e9a642b
Evax Software <contact@evax.fr>
2014-07-14 15:09:18 +0200

    Cleanup dead code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,16 +7,9 @@ module ApplicationHelper
     "<i class=\"fa fa-#{name}\"></i>".html_safe
   end
 
-  def configuration_tab?
-    not ["home", "disbursements"].member? controller_name
-  end
-
   def nan_to_zero(n)
     return 0 if n == 'NaN'
     return n
   end
 
-  def ports_active?
-    ['ports', 'tariffs', 'services', 'terminals'].member? controller_name
-  end
 end
