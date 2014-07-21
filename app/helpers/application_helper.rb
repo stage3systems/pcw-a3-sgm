9503c870bd37c0daca0cf0708806dbd5db98299e
Evax Software <contact@evax.fr>
2014-07-21 20:13:33 +0200

    Remove dead code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,6 @@ module ApplicationHelper
     "<i class=\"glyphicon glyphicon-#{name}\"></i>".html_safe
   end
 
-  def fa_icon(name)
-    "<i class=\"fa fa-#{name}\"></i>".html_safe
-  end
-
   def nan_to_zero(n)
     return 0 if n == 'NaN'
     return n
