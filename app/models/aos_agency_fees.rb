3daad07dfbdf54e1a14591fbce03c0e311c8ff1c
Evax Software <contact@evax.fr>
2014-12-30 09:51:00 +0100

    Minor cleanup

--- app/models/aos_agency_fees.rb
+++ app/models/aos_agency_fees.rb
@@ -15,19 +15,24 @@ class AosAgencyFees
     return fees unless @disbursement.company
     i = offset
     @api.each('agencyFee', {companyId: @disbursement.company.remote_id}) do |f|
-      if f['portId'].nil? or f['portId'] == @disbursement.port.remote_id
+      port_id = f['portId']
+      if port_id.nil? or port_id == @disbursement.port.remote_id
         key = "AGENCY-FEE-#{f['id']}"
         fees["fields"][key] = i
         fees["compulsory"][key] = false
-        c = "{compute: function(ctx) {return #{f['amount']};}, taxApplies: true}"
-        fees["codes"][key] = c
+        fees["codes"][key] = make_js(f['amount'])
         fees["descriptions"][key] = f['title']
         hint = f['description']
-        hint += ' (Port Specific Fee)' if f['portId'] == @disbursement.port.remote_id
+        hint += ' (Port Specific Fee)' if port_id
         fees["hints"][key] = hint
         i += 1
       end
     end
     fees
+  end
+
+  private
+  def make_js(amount)
+    "{compute: function(ctx) {return #{amount};}, taxApplies: true}"
   end
 end
