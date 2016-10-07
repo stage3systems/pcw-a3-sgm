84dfe39040f87bdb1667902785456fb353a73f8e
Evax Software <contact@evax.fr>
2016-10-07 17:29:55 +0200

    Add DWT filtering to the dashboard

--- app/models/disbursement_search.rb
+++ app/models/disbursement_search.rb
@@ -7,6 +7,7 @@ class DisbursementSearch
     filter_association(Terminal)
     filter_cargo_type
     filter_dates
+    filter_dwt
     filter_vessel_name
     @count = @disbursements.count
     @disbursements = @disbursements.order('updated_at DESC')
@@ -42,6 +43,14 @@ class DisbursementSearch
     end_date = Date.parse(@params[:end_date]) rescue nil
     @disbursements = @disbursements.where('disbursements.updated_at > :start_date', start_date: start_date) if start_date
     @disbursements = @disbursements.where('disbursements.updated_at < :end_date', end_date: end_date) if end_date
+  end
+
+  def filter_dwt
+    min_dwt = @params[:min_dwt].to_f unless @params[:min_dwt].blank?
+    max_dwt = @params[:max_dwt].to_f unless @params[:max_dwt].blank?
+    puts "MAX MIN #{min_dwt}, #{max_dwt} #{@params}"
+    @disbursements = @disbursements.where("(disbursement_revisions.data -> 'vessel_dwt')::float >= :min_dwt", min_dwt: min_dwt) if min_dwt
+    @disbursements = @disbursements.where("(disbursement_revisions.data -> 'vessel_dwt')::float <= :max_dwt", max_dwt: max_dwt) if max_dwt
   end
 
   def filter_vessel_name
