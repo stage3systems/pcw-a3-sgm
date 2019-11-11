8dfb497e69a31d7bf9f89cb17093c2ea2eaa92ac
Akhtaab <akhtaab@stage3systems.com>
2019-11-11 11:34:47 +0000

    [AOS-3441] syntax change

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -44,7 +44,7 @@ class Tenant < ActiveRecord::Base
   end
 
   def supports_named_services?
-    name.starts_with? "sgm" || "wallem"
+    ["sgm", "wallem", "biehl"].member? self.customer_name
   end
 
   def supports_free_text_items?
