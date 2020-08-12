5ecef867676b5e50c456a6778a16c9747004a635
Hubert Marzec <marzec@8powers.pl>
2020-08-12 22:37:34 +0200

    A3-589| PDA document formatting should not break when reference section exceeds 3 lines.

--- app/controllers/disbursements_controller.rb
+++ app/controllers/disbursements_controller.rb
@@ -32,7 +32,7 @@ class DisbursementsController < ApplicationController
 
     respond_to do |format|
       format.html { @pfda_view.anonymous!; render layout: "published" }
-      format.pdf { @pfda_view.pdf!; handle_pdf }
+      format.pdf {  handle_pdf }
       format.xls { handle_xls }
     end
   end
@@ -198,7 +198,9 @@ class DisbursementsController < ApplicationController
     unless File.exists? file
       PdfDA.new(@document, root_url).render_file file
     end
-    send_file(file, :type => "application/pdf")
+    PdfDA.new(@document, root_url).render_file file
+    render json: params
+    send_file(file, :type => "application/pdf", :disposition => params[:inline].present? ? 'inline' : 'attachment')
   end
 
   def handle_xls
