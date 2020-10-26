5badf841db9ce31b9951bfe1920afa0f577f883a
Hubert Marzec <marzec@8powers.pl>
2020-10-26 12:49:34 +0100

    A3-673| move init

--- app/models/aos_sync_queue.rb
+++ app/models/aos_sync_queue.rb
@@ -9,15 +9,11 @@ class AosSyncQueue
   end
 
   def publish(entity, data)
-    credentials = Aws::InstanceProfileCredentials.new(retries: 3)
-    client = Aws::SNS::Client.new(
-      region: @region,
-      credentials: credentials
-    )
-    sns = Aws::SNS::Resource.new(client: client)
+    sns = Aws::SNS::Resource.new
     topic = sns.topic(@topicArn)
 
     paylaod = self.prepare_data(entity, data);
+    
     topic.publish({
       message: paylaod.to_json,
       message_attributes: {
---
+++ config/initializers/aws.rb
@@ -0,0 +1,5 @@
+config = Rails.application.config.x.sns
+Aws.config.update(
+  region: config['region'],
+  credentials: Aws::InstanceProfileCredentials.new(retries: 3)
+)
